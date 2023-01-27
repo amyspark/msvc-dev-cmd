516450a3a6da870160ca3c5955fc4ce3e6cc61bc
L. E. Segovia <amy@amyspark.me>
2023-01-26 22:28:15 -0300

    Allow targeting the Preview edition

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ struct Opt {
     args: Vec<PathBuf>,
 }
 
-const EDITIONS: [&str; 3]  = ["Enterprise", "Professional", "Community"];
+const EDITIONS: [&str; 4]  = ["Enterprise", "Professional", "Community", "Preview"];
 
 const YEARS: [&str; 4] = ["2022", "2019", "2017", "2015"];
 
